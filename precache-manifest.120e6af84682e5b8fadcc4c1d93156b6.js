3354e3b9e0758e396f5f4c4d4160fa384df41714
Chris Mills <chris.mills@gorillalogic.com>
2020-07-26 17:47:03 -0700

    updated build

---
+++ precache-manifest.120e6af84682e5b8fadcc4c1d93156b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fde0ec6944e4e80fc6e6f3afa07a444",
+    "url": "./index.html"
+  },
+  {
+    "revision": "356e1cf398c700e58f74",
+    "url": "./static/css/2.3c515868.chunk.css"
+  },
+  {
+    "revision": "1b4bf4b308035cd3a7bb",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "356e1cf398c700e58f74",
+    "url": "./static/js/2.6c9b2cc7.chunk.js"
+  },
+  {
+    "revision": "6b7c2beab9d209c8699dccecdbd544e8",
+    "url": "./static/js/2.6c9b2cc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b4bf4b308035cd3a7bb",
+    "url": "./static/js/main.59587143.chunk.js"
+  },
+  {
+    "revision": "bc6c56a1ec6eb94d9dcd",
+    "url": "./static/js/runtime-main.585b451d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.727352a0b93c6cf031860c220faae63d.js"
+  "./precache-manifest.120e6af84682e5b8fadcc4c1d93156b6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
