4356b9e12eac0702809c8827de930c218b52a3ae
Chris Mills <chris.mills@gorillalogic.com>
2020-07-26 17:59:16 -0700

    update build

---
+++ precache-manifest.727352a0b93c6cf031860c220faae63d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "132e2ddbc0705249682f6bd596b8bf1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e20fb2f5cdb908180fa",
+    "url": "/static/css/2.3c515868.chunk.css"
+  },
+  {
+    "revision": "e7fc80c7000579a22ca5",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0e20fb2f5cdb908180fa",
+    "url": "/static/js/2.2f90ec48.chunk.js"
+  },
+  {
+    "revision": "6b7c2beab9d209c8699dccecdbd544e8",
+    "url": "/static/js/2.2f90ec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7fc80c7000579a22ca5",
+    "url": "/static/js/main.510b0a59.chunk.js"
+  },
+  {
+    "revision": "e92cf1c5f3f45918ce01",
+    "url": "/static/js/runtime-main.78023402.js"
+  }
+]);
